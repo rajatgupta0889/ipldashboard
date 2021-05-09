e6a9bbacdc2baaca1f52d8ab13a298ac84f5e421
rajat <rajatgupta0889@gmail.com>
2021-05-09 23:59:33 +0530

    Add matches api

--- src/main/java/io/tech/ipldashboard/ipldashboard/controller/TeamController.java
+++ src/main/java/io/tech/ipldashboard/ipldashboard/controller/TeamController.java
@@ -1,11 +1,16 @@
 package io.tech.ipldashboard.ipldashboard.controller;
 
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.tech.ipldashboard.ipldashboard.model.Match;
 import io.tech.ipldashboard.ipldashboard.model.Team;
 import io.tech.ipldashboard.ipldashboard.repository.MatchRepository;
 import io.tech.ipldashboard.ipldashboard.repository.TeamRepository;
@@ -30,5 +35,13 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatches(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year,1,1);
+        LocalDate endDate = LocalDate.of(year+1,1,1);
+        return this.matchRepository.getMatchesByTeamBetweenDate(teamName, startDate, endDate);
+        
     }
 }
