342de89149dde743ced42eb6a98c8ccfb70981da
rajat <rajatgupta0889@gmail.com>
2021-05-10 00:06:53 +0530

    Add match page in react app

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -4,26 +4,26 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
-    // const [team, setTeam] = useState({ matches: [] });
-    // const { teamName } = useParams();
-    // useEffect(
-    //     () => {
-    //         const fetchMatches = async () => {
-    //             // Chennai%20Super%20Kings
-    //             // const response = await fetch(`http://localhost:8080/team/${teamName}`)
-    //             // const data = await response.json();
-    //             // setTeam(data);
-    //             // console.log(data)
-    //         }
-    //         fetchMatches();
-    //     }, [teamName]
-    // );
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                // Chennai%20Super%20Kings
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`)
+                const data = await response.json();
+                setMatches(data);
+                console.log(data)
+            }
+            fetchMatches();
+        }, [teamName, year]
+    );
     // if (!team || !team.teamName) return <h1> Team Not found</h1>
     return (
         <div className="MatchPage">
             <h1> MatchPage</h1>
-            {/* <MatchDetailCard match={team.matches[0]} teamName={team.teamName} />
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} key={match.id} teamName={team.teamName}/>)} */}
+            {/* <MatchDetailCard match={team.matches[0]} teamName={team.teamName} /> */}
+            {matches.map(match => <MatchDetailCard match={match} key={match.id} teamName={teamName}/>)}
         </div>
     );
 }
