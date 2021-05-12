42fadd8695629c86b053b8b7e4765cc96c46e43f
rajat <rajatgupta0889@gmail.com>
2021-05-12 23:31:21 +0530

    Styled team page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import { MatchPage } from './pages/MatchPage';
 import { TeamPage } from './pages/TeamPage';
 
--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,7 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
     const [matches, setMatches] = useState([]);
