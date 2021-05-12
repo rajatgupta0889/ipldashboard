479c8efd1e4d28d813efe1e7af98b4ec2509ab48
rajat <rajatgupta0889@gmail.com>
2021-05-12 23:31:00 +0530

    Styled the team page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,7 +2,9 @@ import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
 
+import './TeamPage.scss';
 export const TeamPage = () => {
     const [team, setTeam] = useState({ matches: [] });
     const { teamName } = useParams();
@@ -21,9 +23,29 @@ export const TeamPage = () => {
     if (!team || !team.teamName) return <h1> Team Not found</h1>
     return (
         <div className="TeamPage">
-            <h1> {team.teamName} </h1>
-            <MatchDetailCard match={team.matches[0]} teamName={team.teamName} />
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} key={match.id} teamName={team.teamName}/>)}
+            <div className="team-name-section">
+                <h1 className="team-name"> {team.teamName} </h1>
+            </div>
+            <div className="win-loss-section">
+                Wins / losses
+                 <PieChart
+                    data={[
+                        { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+                        { title: 'Wins', value: team.totalWins, color: '#4da375' },
+
+                    ]}
+                />
+            </div>
+            <div className="match-detail-section">
+                <h3> Latest Matches </h3>
+                <MatchDetailCard match={team.matches[0]} teamName={team.teamName} />
+            </div>
+            {team.matches.slice(1).map(match => <MatchSmallCard match={match} key={match.id} teamName={team.teamName} />)}
+            <div className="more-link">
+                <a href="#">More ></a>
+            </div>
+
+
         </div>
     );
 }
